541271db9561858569232aa46aa223b2850912df
Clover Dev <cloverdev88@gmail.com>
2021-06-11 13:47:47 -0400

    Added News Landing

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -3,12 +3,14 @@ import LayoutDefault from 'components/LayoutDefault'
 import Header from './components/Header'
 import Main from './components/Main'
 import Text from './components/Text'
+import NewsModal from './components/NewsModal'
 import Footer from './components/Footer'
 
 const Home: React.FC = () => {
   return (
     <LayoutDefault>
       <Header />
+      <NewsModal />
       <Main />
       <Text />
       <Footer />
